fce23c9d465d4e0357b2df6c3c84b737f4b4e92f
Deepraj Das <deepraajdas@gmail.com>
2026-01-18 23:31:45 +0530

    feat: add AppColors class for color constants

---
+++ src/core/constants/colors.ts
@@ -0,0 +1,11 @@
+class AppColors {
+    static bgColor = "#3A3636";
+    static buttonColorLight = "#9E9D9D";
+    static buttonColorDark = "#615E5E";
+
+    static cardBgColor = "#615E5E";
+
+
+}
+
+export default AppColors
