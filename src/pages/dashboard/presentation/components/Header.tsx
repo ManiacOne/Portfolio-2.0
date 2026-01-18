4e392cc1e722a2fb262aeafff45cf329b1b614fc
Deepraj Das <deepraajdas@gmail.com>
2026-01-18 23:32:00 +0530

    feat: add Header component with styles for layout and content

---
+++ src/pages/dashboard/presentation/components/Header.tsx
@@ -0,0 +1,13 @@
+import '../styles/header.scss'
+
+const Header = () => {
+
+    return <div className='header_root'>
+        <p className='left_content'>@code by Deepraj</p>
+        <p className='right_content'>
+            Passionate Creative Designer and Developer, dedicated to crafting innovative solutions and exceptional digital experiences through modern technologies
+        </p>
+    </div>
+}
+
+export default Header
