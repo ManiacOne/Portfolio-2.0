7a4ac8c6b2c42754fd3aff1d735de17d506595c4
Deepraj Das <deepraajdas@gmail.com>
2026-01-18 23:31:03 +0530

    feat: assets added

--- src/core/constants/assets_images.ts
+++ src/core/constants/assets_images.ts
@@ -9,24 +9,10 @@ import iphoneMockUp from '../../assets/demp_iphone_mockup.mp4';
 import iphoneMockUp2 from '../../assets/demp_iphone_mockup2.mp4';
 import xxBunny from '../../assets/xx_bunny.svg';
 import kawsBunny from '../../assets/kaws_bunny.svg';
-import heroImage from '../../assets/hero_image.png';
-import heroImage3 from '../../assets/hero_image_3.png';
-import heroImage4 from '../../assets/hero_image_4.png';
-import heroImage5 from '../../assets/hero_image_5.jpeg';
-import heroImage6 from '../../assets/hero_image_6.png';
-import heroImageUpscaled from '../../assets/hero_image_upscaled.png';
-import heroImageWithoutBG from '../../assets/hero_image_without_bg.png';
-import heroImage4k from '../../assets/hero-image-4k.png';
+import heroImage from '../../assets/hero_image_6.png';
 
 export {
     heroImage,
-    heroImageUpscaled,
-    heroImageWithoutBG,
-    heroImage4k,
-    heroImage3,
-    heroImage4,
-    heroImage5,
-    heroImage6,
     logo,
     logoBlack,
     ghost,
