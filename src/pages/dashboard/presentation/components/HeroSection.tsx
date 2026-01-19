46a9afb81bb7f8a1ea7f5c16286c7fc68d0be201
Deepraj Das <deepraajdas@gmail.com>
2026-01-19 09:22:11 +0530

    feat: refactor HeroSection component for improved layout and animation integration

--- src/pages/dashboard/presentation/components/HeroSection.tsx
+++ src/pages/dashboard/presentation/components/HeroSection.tsx
@@ -3,24 +3,20 @@ import { useHeroSectionAnimation } from '../hooks/useHeroSectionAnimation';
 import '../styles/heroSection.scss';
 
 const HeroSection = () => {
-  const { dashboardConainerRef, imageRef, trailRef } = useHeroSectionAnimation();
+  const { dashboardConainerRef, imageRef, trailRef, headerRef } = useHeroSectionAnimation();
 
   return (
     <section ref={dashboardConainerRef} className="hero">
-      <div className="header_root">
-        <p className="left_content">@code by Deepraj</p>
-        <p className="right_content">
-          Passionate Creative Designer and Developer, dedicated to crafting innovative solutions and
-          exceptional digital experiences through modern technologies
-        </p>
-      </div>
+      <p ref={headerRef} className="header_root">
+        code by Deepraj
+        <br />
+        Passionate Creative Designer and Developer, dedicated to crafting innovative solutions and
+        exceptional digital experiences through modern technologies.
+      </p>
 
       <img ref={imageRef} src={heroImage} alt="Maniac" className="hero_image" />
 
-      <div
-        ref={trailRef}
-        className='hero_trail'
-      >
+      <div ref={trailRef} className="hero_trail">
         {[1, 2, 3, 4].map((index) => (
           <span key={index} className="trail_line">
             {'DEVELOPER●'.split('').map((char, charIndex) => (
