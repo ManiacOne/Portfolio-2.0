81e0057416c8bdde38a816a9ac04c477f2da3c84
Deepraj Das <deepraajdas@gmail.com>
2026-02-02 01:40:08 +0530

    feat: update Dashboard layout and styles for improved structure and responsiveness

--- src/pages/dashboard/presentation/screens/Dashboard.tsx
+++ src/pages/dashboard/presentation/screens/Dashboard.tsx
@@ -1,13 +1,20 @@
-import HeroSection from '../components/HeroSection';
-import Projects from '../components/Projects';
+import Experience from '../components/experience/Experience';
+import HeroSection from '../components/heroSection/HeroSection';
 import { ReactLenis } from '@studio-freight/react-lenis';
+import ProjectsV2 from '../components/projectsV2/ProjectsV2';
+import GetInTouch from '../components/getInTouch/GetInTouch';
 
 const Dashboard = () => {
   return (
     <ReactLenis root options={{ lerp: 0.05 }}>
       <div className="dashboard_root">
         <HeroSection />
-        <Projects />
+        {/* <Projects /> */}
+        <ProjectsV2 />
+        <Experience />
+        <GetInTouch />
+        {/* <Demo />
+        <DemoAdvanced /> */}
       </div>
     </ReactLenis>
   );
